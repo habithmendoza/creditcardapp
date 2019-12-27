22a189cb997952855a656152ccca5c5426e73bda
habimendoza <habimendoza@hotmail.com>
2019-12-27 00:57:02 -0500

    Updates

---
+++ precache-manifest.bed80fa1a5ceadf2e1eee803bbc0ffa5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36777780945d2d0f34a79a7f0443c851",
+    "url": "/creditcardapp/index.html"
+  },
+  {
+    "revision": "35a55e4e84ae35755f3f",
+    "url": "/creditcardapp/static/css/main.7d44a203.chunk.css"
+  },
+  {
+    "revision": "45b4e83c11974ca477ab",
+    "url": "/creditcardapp/static/js/2.b5a20263.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/creditcardapp/static/js/2.b5a20263.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35a55e4e84ae35755f3f",
+    "url": "/creditcardapp/static/js/main.b45fbde4.chunk.js"
+  },
+  {
+    "revision": "0e3530275dc6db3f12be",
+    "url": "/creditcardapp/static/js/runtime-main.2020c811.js"
+  },
+  {
+    "revision": "0841af952c807bdf56455b1addb4c7df",
+    "url": "/creditcardapp/static/media/IndieFlower-R.0841af95.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/creditcardapp/static/media/Montserrat-L.409c7f79.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/creditcardapp/static/media/Montserrat-R.ee653992.ttf"
+  },
+  {
+    "revision": "6db01fa9d74dac090b3ffa714de1ea7e",
+    "url": "/creditcardapp/static/media/chip.6db01fa9.png"
+  },
+  {
+    "revision": "e67b3b21fc722442cca782e273ab90d0",
+    "url": "/creditcardapp/static/media/creditcardBack.e67b3b21.jpg"
+  },
+  {
+    "revision": "c62cfabca0d795b7c035e05bc023cadd",
+    "url": "/creditcardapp/static/media/creditcardFront.c62cfabc.jpg"
+  },
+  {
+    "revision": "fe807bce353d0bc60f09a60409236255",
+    "url": "/creditcardapp/static/media/mastercard.fe807bce.png"
+  },
+  {
+    "revision": "89225e85b362eb53dc335464430d3915",
+    "url": "/creditcardapp/static/media/visa.89225e85.png"
+  }
+]);
